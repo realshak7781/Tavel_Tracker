957b4d1999c4376ccada85718d6517301ea2064b
realshak7781 <124501803+realshak7781@users.noreply.github.com>
2024-06-24 05:35:37 +0530

    DB connected and started with some initial values .No input ttaken yet

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db =new pg.Client({
+  user: 'postgres',
+  password: '973743_k_s',
+  host: 'localhost',
+  port: 5432,
+  database: 'world',
+});
+
+db.connect();
 
 const app = express();
 const port = 3000;
@@ -7,8 +18,23 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
 app.get("/", async (req, res) => {
-  //Write your code here.
+  const result=await db.query("SELECT country_code FROM visited_countries");
+  let countries=[];
+
+  result.rows.forEach((country)=>{
+    countries.push(country.country_code);
+  })
+
+  console.log(result.rows);
+  res.render("index.ejs",{
+    countries: countries,
+    total:countries.length
+  });
+
+  db.end();
+  
 });
 
 app.listen(port, () => {
